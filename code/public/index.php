8ce1aab8443a478677329898ca2ea7f18dc0025a
nikcrazy37 <nik_crazy@bk.ru>
2023-03-25 15:58:38 +0300

    move to server

--- code/public/index.php
+++ code/public/index.php
@@ -6,6 +6,10 @@ const ROOT = __DIR__ . "/..";
 
 require ROOT . "/vendor/autoload.php";
 
+//echo '123';
+
 use Fluent\Logger\FluentLogger;
 $logger = new FluentLogger("clickhouse","8123");
-$logger->post("fluentd.test.follow", array("from"=>"userA", "to"=>"userB"));
+$res = $logger->post("fluentd.test.follow", array("from"=>"userA", "to"=>"userB"));
+
+print_r($res);
