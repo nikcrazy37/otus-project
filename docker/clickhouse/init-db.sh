165794f440c8b57748d7ecb842342c40cc2830c5
nikcrazy37 <nik_crazy@bk.ru>
2023-06-06 11:21:41 +0300

    save commit

--- docker/clickhouse/init-db.sh
+++ docker/clickhouse/init-db.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 clickhouse client -n <<-EOSQL
+    SET allow_experimental_object_type = 1;
     CREATE TABLE IF NOT EXISTS default.fluentd (time DateTime(), tag String, data String) ENGINE = MergeTree ORDER BY time;
 EOSQL
